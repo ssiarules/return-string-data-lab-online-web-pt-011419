38d464c91cfd0449995785a9d4c81380075fac4e
Johnson Joseph <jhonsonj88@gmail.com>
2019-09-07 23:19:50 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,15 @@
 class ProductsController < ApplicationController
 
+  def index
+      @products = Product.all
+    end
+
+    def new
+       @product = Product.new
+     end
+
+
+
   def create
     @product = Product.create(product_params)
     @product.save
@@ -13,7 +23,7 @@ class ProductsController < ApplicationController
   def description
       render plain: @product.description
     end
-    
+
   private
   def product_params
      params.require(:product).permit(:name, :description, :inventory)
