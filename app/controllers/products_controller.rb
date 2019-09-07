6dab2548ac02c8b89e1641b54d93920bf63e899b
Johnson Joseph <jhonsonj88@gmail.com>
2019-09-07 23:11:22 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,14 @@
 class ProductsController < ApplicationController
 
+  def create
+    @product = Product.create(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+     params.require(:product).permit(:name, :description, :inventory)
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home', to: 'static#home'
+
+
+  resources :products, only: [:index, :show, :new, :create, :edit]
+  get '/products/:id/inventory', to: 'products#inventory'
   resources :orders
   resources :invoices
 end
