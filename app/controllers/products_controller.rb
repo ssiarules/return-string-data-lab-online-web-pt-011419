f9172591edba686eac94b9ccb08d143c839a3d9f
Johnson Joseph <jhonsonj88@gmail.com>
2019-09-07 23:11:49 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,9 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def inventory 
+  end
+
   private
   def product_params
      params.require(:product).permit(:name, :description, :inventory)
